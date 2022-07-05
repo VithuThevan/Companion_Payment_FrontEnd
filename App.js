172aada1ad07d712bcb7ef620b28367dfe34262c
Vithursan1003 <vithursan1003@gmail.com>
2022-07-05 13:47:01 +0530

    Did some changes

--- App.js
+++ App.js
@@ -15,21 +15,23 @@ const options = {
 };
 
 const App = () => {
-
   const openGallery = async () => {
     const images = await launchImageLibrary(options);
     console.log(images.assets[0].type);
-      if (!images.assets[0].type.match("image/png")) {
-          if (!images.assets[0].type.match("image/jpeg")){
-              if (!images.assets[0].type.match("image/jpg")) {
-                  let uploadfail =  new createTwoButtonAlert("Upload Failed!","The Image format is not in png/jpg/jpeg format!");
-                  uploadfail;
-                  return false;
-              }
-          }
+    if (!images.assets[0].type.match('image/png')) {
+      if (!images.assets[0].type.match('image/jpeg')) {
+        if (!images.assets[0].type.match('image/jpg')) {
+          let uploadfail = new createTwoButtonAlert(
+            'Upload Failed!',
+            'The Image format is not in png/jpg/jpeg format!',
+          );
+          uploadfail;
+          return false;
+        }
       }
+    }
 
-      const formdata = new FormData();
+    const formdata = new FormData();
     formdata.append('file', {
       uri: images.assets[0].uri,
       type: images.assets[0].type,
@@ -43,37 +45,31 @@ const App = () => {
         'Content-Type': 'multipart/form-data',
       },
     });
-    let buttonalert =  new createTwoButtonAlert("Success","Your Image has been Uploaded!");
+    let buttonalert = new createTwoButtonAlert(
+      'Success',
+      'Your Payment receipt has been sent for verification',
+    );
     buttonalert;
     let responseJson = await res.json();
     console.log(responseJson, 'responseJson');
   };
 
   const createTwoButtonAlert = (message1, message2) =>
-      Alert.alert(
-          message1,
-          message2,
-          [
-            {
-              text: "Cancel",
-              onPress: () => console.log("Cancel Pressed"),
-              style: "cancel"
-            },
-            { text: "OK", onPress: () => console.log("OK Pressed") }
-          ]
-      );
+    Alert.alert(message1, message2, [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
 
   return (
-    <View
-      style={{
-        flex: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{flex: 20, alignItems: 'center', justifyContent: 'center'}}>
       <Button title="Uploads" onPress={openGallery} />
     </View>
   );
-
 };
 
 export default App;
